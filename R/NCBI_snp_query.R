06efefe8d2385aa36c9e24c1eaaf409844e8292e
Sina Rueeger <sina.rueeger@gmail.com>
2019-07-27 15:24:44 +0200

    making further changes so that one snps works.

--- R/NCBI_snp_query.R
+++ R/NCBI_snp_query.R
@@ -102,8 +102,8 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
 
   
   ## we don't need the last element; it's just metadata
-  xml_list <- xml_list_[ 1:(length(xml_list_) - 1) ]
-  return(xml_list)
+  xml_list <- xml_list_#[ 1:(length(xml_list_) - 1) ]
+ # return(xml_list)
   
 
   ## Check which rs numbers were found, and warn if one was not found
@@ -112,7 +112,7 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
   found_snps <- unname( unlist( sapply( xml_list, function(x) {
 
     ## check if the SNP is either in the current rsId, or the merged SNP list
-    attr_rsIds <- tryget( x$DocumentSummarySet$DocumentSummary$SNP_ID)
+    attr_rsIds <- tryget( x$DocumentSummary$SNP_ID )
 
     ## TODO: figure out where this part has moved in the xml file
     merge_indices <- which( names(x) == "MergeHistory" )
@@ -143,49 +143,62 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
 
   for (i in seq_along(SNPs)) {
     my_list <- xml_list[[i]]
-    my_chr <- tryget(my_list$Assembly$Component$.attrs["chromosome"])
+    my_chr <- tryget(my_list$DocumentSummary$CHR)
     if (is.null(my_chr)) {
       my_chr <- NA
       warning("No chromosomal information for ", SNPs[i], "; may be unmapped", 
               call. = FALSE)
     }
-    my_snp <- tryget( my_list$.attrs["rsId"] )
+    my_snp <- tryget(my_list$DocumentSummary$SNP_ID )
     if ( !is.na(my_snp) ) {
       my_snp <- paste(sep = '', "rs", my_snp)
     }
     if (my_snp != SNPs[i] ) {
       warning(SNPs[i], " has been merged into ", my_snp, call. = FALSE)
     }
-    my_snpClass <- tryget(my_list$.attrs["snpClass"])
+    my_snpClass <- tryget(my_list$DocumentSummary$SNP_CLASS)
 
-    my_gene <- xml2::xml_attr(
-      xml2::xml_find_first(x2kids[[i]], "Assembly//Component/MapLoc/FxnSet"),
+    my_gene <- tryget(my_list$DocumentSummary$GENES$GENE_E$NAME)
+    # xml2::xml_attr(
+  #    xml2::xml_find_first(x2kids[[i]], "Assembly//Component/MapLoc/FxnSet"),
       "symbol"
-    )
+  #  )
     # my_gene <- tryget( my_list$Assembly$Component$MapLoc$FxnSet['symbol'] )
     if (is.null(my_gene)) my_gene <- NA
-    alleles <- my_list$Ss$Sequence$Observed
+      
+    meta_info_ <- tryget(my_list$DocumentSummary$DOCSUM) 
+    meta_info <- strsplit(meta_info_, "=|:|,")[[1]][3]
 
+    alleles_ordered <- gsub("g.|[0-9]", "", meta_info)
+    alleles <- strsplit(alleles_ordered, ">")[[1]]
+    ancestral_allele <- alleles[1]
+    variation_allele <- alleles[2]
+      
     ## handle true SNPs
     if (my_snpClass %in% c("snp", "snv")) {
-      tmp <- c( my_list$Ss$Sequence$Observed, my_list$Ss$.attrs["orient"] )
-      if (tmp[2] != "forward") {
-        tmp[1] <- flip( tmp[1], sep = "/" )
-      }
-      alleles_split <- strsplit( tmp[1], "/" )[[1]]
+      
+
+      ## TODO: fix this
+     # tmp <- c( my_list$Ss$Sequence$Observed, my_list$Ss$.attrs["orient"] )
+#      if (tmp[2] != "forward") {
+#        tmp[1] <- flip( tmp[1], sep = "/" )
+#      }
+     # alleles_split <- strsplit( tmp[1], "/" )[[1]]
 
       ## check which of the two alleles grabbed is actually the minor allele
       ## we might have to 'flip' the minor allele if there is no match
-      maf_allele <- my_list$Frequency['allele']
+      maf_allele <- my_list$DocumentSummary$GLOBAL_MAFS$MAF$FREQ
+      maf_allele_dissect <- strsplit(maf_allele, "=|/")[[1]]
+      
       if (is.null(maf_allele)) {
-        maf_allele <- alleles <- strsplit(my_list$Sequence$Observed, "/")[[1]]
-        my_major <- alleles[1]
-        my_minor <- alleles[2]
+        maf_allele <- NA
+        my_major <- NA
+        my_minor <- NA
         my_freq <- NA
       } else {
-        my_minor <- alleles_split[ maf_allele == alleles_split ]
-        my_major <- alleles_split[ maf_allele != alleles_split ]
-        my_freq <- my_list$Frequency["freq"]
+        my_minor <- maf_allele_dissect[1]
+        my_major <- ifelse(my_minor == variation_allele, ancestral_allele , variation_allele)
+        my_freq <- maf_allele_dissect[2]
       }
     } else { 
       ## handle the others in a generic way; maybe specialize later
@@ -194,10 +207,12 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
       my_freq <- NA
     }
 
-    my_pos <- xml2::xml_attr(
-      xml2::xml_find_first(x2kids[[i]], "Assembly//Component/MapLoc[@physMapInt]"), 
-      "physMapInt"
-    )
+    my_pos <- tryget(my_list$DocumentSummary$CHRPOS)
+    my_pos <- strsplit(my_pos, ":")[[1]][2]
+    #xml2::xml_attr(
+    #  xml2::xml_find_first(x2kids[[i]], "Assembly//Component/MapLoc[@physMapInt]"), 
+    #  "physMapInt"
+    #)
     # my_pos <- tryCatch(
     #   my_list$Assembly$Component$MapLoc$.attrs["physMapInt"],
     #   error = function(e) {
@@ -211,10 +226,11 @@ ncbi_snp_query <- function(SNPs, key = NULL, ...) {
     # should be off by one when compared to web display. So we add one here
     # to make them equivalent 
     #if (is.numeric(my_pos)) my_pos <- my_pos + 1
-    my_pos <- as.integer(my_pos) + 1
+    ## TODO: check if the +1 is still needed
+    my_pos <- as.integer(my_pos) #+ 1
     
     # Ancestral Allele
-    anc_all <- my_list$Sequence$.attrs['ancestralAllele']
+    anc_all <- ancestral_allele
     anc_all <- if (is.na(anc_all)) NA else anc_all[[1]]
 
     out[i, ] <- c(
